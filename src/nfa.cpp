02aa4721d25e4bef63e1198e400c5d72d0c90f09
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-04-27 11:00:57 +0300

    Implement conversion from nfa to dfa

--- src/nfa.cpp
+++ src/nfa.cpp
@@ -1,7 +1,10 @@
 #include "nfa.hpp"
 #include "printer.hpp"
+#include "transition.hpp"
 
 #include <algorithm>
+#include <deque>
+#include <map>
 #include <set>
 #include <utility>
 
@@ -77,7 +80,7 @@ auto nfa::print_transitions() -> void
     auto const& autom = m_builder.get_configuration();
 
     for(auto const& [state, transitions] : autom) {
-        std::cout << state << ':' << '\t';
+        std::cout << state << ": ";
         print(transitions);
         std::cout << std::endl;
     }
@@ -86,6 +89,173 @@ auto nfa::print_transitions() -> void
 auto nfa::to_dfa() -> builder
 {
     builder result{};
+    auto const& autom = m_builder.get_configuration();
+    std::deque<std::set<int>> additional_states{};
+    std::map<int, std::map<char, std::set<int>>> new_autom{};
+    int max_state{ 0 };
+    int max_state_copy{ 0 };
+
+    auto already_seen = [&additional_states](std::set<int> const& set) {
+        return std::find(additional_states.begin(),
+                         additional_states.end(),
+                         set) != additional_states.end();
+    };
+
+    for(auto const& [state, transitions] : autom) {
+        if(state > max_state) {
+            max_state = state;
+            max_state_copy = max_state;
+        }
+
+        for(char const ch : m_builder.get_alphabet()) {
+            std::set<int> path{};
+
+            for(auto const& transition : transitions) {
+                if(transition.on == ch) {
+                    path.insert(transition.to);
+                }
+            }
+
+            if(path.size() > 1U && !already_seen(path)) {
+                additional_states.push_back(path);
+            }
+
+            new_autom[state][ch] = path;
+        }
+    }
+
+    std::cout << "\nAdditional states:\n";
+    for(auto const& set : additional_states) {
+        print(set);
+        std::cout << ' ';
+    }
+    std::cout << std::endl << std::endl;
+
+    std::cout << "New autom:\n";
+    for(auto const& [state, transitions] : new_autom) {
+        std::cout << state << ":\n";
+
+        for(auto const& [ch, path] : transitions) {
+            std::cout << ch << " -> ";
+            print(path);
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
+    std::deque<std::set<int>> additional_states_copy{ additional_states };
+
+    while(!additional_states_copy.empty()) {
+        auto const& to_check = additional_states_copy.front();
+        ++max_state;
+
+        for(char const ch : m_builder.get_alphabet()) {
+            std::set<int> path{};
+            for(int const state : to_check) {
+                for(auto const& transition : autom.at(state)) {
+                    if(transition.on == ch) {
+                        path.insert(transition.to);
+                    }
+                }
+            }
+
+            if(path.size() > 1U && !already_seen(path)) {
+                additional_states.push_back(path);
+                additional_states_copy.push_back(path);
+            }
+
+            new_autom[max_state][ch] = path;
+        }
+
+        additional_states_copy.pop_front();
+    }
+
+    std::cout << "\nAdditional states:\n";
+    for(auto const& set : additional_states) {
+        print(set);
+        std::cout << ' ';
+    }
+    std::cout << std::endl << std::endl;
+
+    auto index = [&additional_states,
+                  max_state_copy](std::set<int> const& state) -> int {
+        if(state.empty()) {
+            return -1;
+        }
+        if(state.size() == 1U) {
+            return *state.begin();
+        }
+        auto it = std::find(
+            additional_states.begin(), additional_states.end(), state);
+
+        return static_cast<int>(std::distance(additional_states.begin(), it)) +
+               max_state_copy + 1;
+    };
+
+    std::cout << "New autom:\n";
+    for(auto const& [state, transitions] : new_autom) {
+        std::cout << state << ":\n";
+
+        for(auto const& [ch, path] : transitions) {
+            std::cout << ch << " -> ";
+            if(path.empty()) {
+                std::cout << "{}";
+            }
+            else if(path.size() == 1U) {
+                std::cout << *path.begin();
+            }
+            else {
+                std::cout << index(path);
+            }
+            // print(path);
+            std::cout << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
+    auto is_final = [this](std::set<int> const& set) -> bool {
+        auto const& accepting_states = m_builder.get_accepting_states();
+        for(int const state : set) {
+            auto it = std::find(
+                accepting_states.begin(), accepting_states.end(), state);
+
+            if(it != accepting_states.end()) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    result.set_starting_state(m_builder.get_starting_state());
+
+    auto const& old_final_states = m_builder.get_accepting_states();
+    std::set<int> new_final_states{};
+
+    if(!old_final_states.empty()) {
+        new_final_states.insert(old_final_states.begin(),
+                                old_final_states.end());
+    }
+
+    for(auto const& [state, transitions] : new_autom) {
+        for(auto const& [ch, path] : transitions) {
+            if(path.size() > 1U && is_final(path)) {
+                new_final_states.insert(state);
+            }
+
+            auto const idx = index(path);
+            if(idx > 0) {
+                result.add_transition(state, ch, idx);
+            }
+        }
+    }
+
+    std::cout << "New final states: ";
+    print(new_final_states);
+    std::cout << std::endl;
+    for(auto const& final_state : new_final_states) {
+        result.set_accepting_state(final_state);
+    }
 
     return result;
 }
